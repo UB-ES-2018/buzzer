b927e93faf8c1e1b8e9a604dc5813ac55ec33f1d
NibrassHaider <nhaideha7@alumnes.ub.edu>
2018-12-04 21:08:51 +0100

    WebSockets Configuration and Installing Django-Nose,Coverage

--- EntornBuzzer/settings.py
+++ EntornBuzzer/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels'
+    'channels',
+    'django_nose',
 ]
 
 # Use nose to run all tests
