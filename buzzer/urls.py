7ed2d8c8e5c3037632ac0895e0a3b1a67723baaa
NibrassHaider <nhaideha7@alumnes.ub.edu>
2018-12-06 02:11:10 +0100

    Testing Frontend Sockets

--- buzzer/urls.py
+++ buzzer/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     url(r'^profile/(?P<user>.*)/$', views.profile, name='profile'),
     url(r'^actualizarProfile/(?P<user>.*)/$', views.actualizarProfile, name='actualizarProfile'),
     url(r'^messages/$', views.private_messages, name='messages'),
+    url(r'^message/$', views.conversation, name="chat"),
     url(r'^message/(?P<user>.*)/$', views.conversation, name="chat"),
 
     # Browser DBs
