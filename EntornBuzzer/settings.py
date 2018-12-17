22bf3f96b5db2071bfd5fd7dd95e4caaf42589a5
sergioBarril <sergiobarrilpizarro@gmail.com>
2018-12-17 16:20:09 +0100

    Añadido Debug=False

--- EntornBuzzer/settings.py
+++ EntornBuzzer/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^epfow-jfxoqagm&b+4hqaro=!0tq3@1giwfq^9!ml!u%ndh8a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
